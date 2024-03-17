60ca0d6cfccdf671c3cd5a505f705b1ce101236e
Sinisa <sinisasurlan@gmail.com>
2024-03-17 11:56:09 +0100

    create routes and index.html style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <BrowserRouter>
+        <Box sx = {{backgroundColor:'#000'}}>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" element={<VideoDetali />} />
+            <Route path="/chanel/:id" element={<ChannelDetali />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </div>
   );
 }
